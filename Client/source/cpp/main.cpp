975b78e93c630cda04ec92aac8da2abb28d5aeb9
ggggtxgt <3035886959@qq.com>
2025-12-22 15:27:27 +0800

    001_06使用qss美化

--- Client/source/cpp/main.cpp
+++ Client/source/cpp/main.cpp
@@ -1,9 +1,23 @@
 #include "mainwindow.h"
 
+#include <QFile>
+#include <QDebug>
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
     QApplication application(argc, argv);
+
+    // 加载 qss 文件
+    QFile qss("../res/style/stylesheet.qss");
+    if (qss.open(QFile::ReadOnly)) {
+        qDebug() << "open qss success.";
+        QString style = QLatin1String(qss.readAll());
+        application.setStyleSheet(style);
+        qss.close();
+    } else {
+        qDebug() << "open qss failed.";
+    }
+
     MainWindow w;
     w.show();
     return application.exec();
