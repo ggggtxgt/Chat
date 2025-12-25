85a0935fdd3cca094f8687aa008bea4c435a7d89
ggggtxgt <3035886959@qq.com>
2025-12-25 12:08:36 +0800

    013_01添加redis.js并修改server.js

--- Server/VarifyServer/server.js
+++ Server/VarifyServer/server.js
@@ -1,19 +1,38 @@
 const grpc = require('@grpc/grpc-js')
 const message_proto = require('./proto')
 const const_module = require('./const')
-const { v4: uuidv4 } = require('uuid');
+const {v4: uuidv4} = require('uuid');
 const emailModule = require('./email');
+const redis_module = require('./redis')
 
 // grpc 响应获取验证码的的服务
 // call 其为 grpc 请求
 // callback 其为 grpc 回调
-
 async function GetVarifyCode(call, callback) {
     console.log("email is ", call.request.email)
-    try{
-        uniqueId = uuidv4();
+    try {
+        let query_res = await redis_module.GetRedis(const_module.code_prefix + call.request.email);
+        console.log("query_res is ", query_res)
+        let uniqueId = query_res;
+        if (query_res == null) {
+            uniqueId = uuidv4();
+            if (uniqueId.length > 4) {
+                uniqueId = uniqueId.substring(0, 4);
+            }
+
+            // 只有 key 不存在时，才设置验证码
+            // @todo 用户多次语法，都分别生成不同的验证码
+            let bres = await redis_module.SetRedisExpire(const_module.code_prefix + call.request.email, uniqueId, 600)
+            if (!bres) {
+                callback(null, {
+                    email: call.request.email,
+                    error: const_module.Errors.RedisErr
+                });
+                return;
+            }
+        }
         console.log("uniqueId is ", uniqueId)
-        let text_str =  '您的验证码为'+ uniqueId +'请在三分钟内完成注册'
+        let text_str = '您的验证码为' + uniqueId + '请在三分钟内完成注册'
         // 发送邮件
         let mailOptions = {
             from: 'ggggtxgtwm@163.com',
@@ -25,23 +44,23 @@ async function GetVarifyCode(call, callback) {
         let send_res = await emailModule.SendMail(mailOptions);
         console.log("send res is ", send_res)
 
-        callback(null, { email:  call.request.email,
-            error:const_module.Errors.Success
+        callback(null, {
+            email: call.request.email,
+            error: const_module.Errors.Success
         });
-
-    }catch(error){
+    } catch (error) {
         console.log("catch error is ", error)
 
-        callback(null, { email:  call.request.email,
-            error:const_module.Errors.Exception
+        callback(null, {
+            email: call.request.email,
+            error: const_module.Errors.Exception
         });
     }
-
 }
 
 function main() {
     var server = new grpc.Server()
-    server.addService(message_proto.VarifyService.service, { GetVarifyCode: GetVarifyCode })
+    server.addService(message_proto.VarifyService.service, {GetVarifyCode: GetVarifyCode})
     server.bindAsync('127.0.0.1:50051', grpc.ServerCredentials.createInsecure(), () => {
         server.start()
         console.log('grpc server started')
